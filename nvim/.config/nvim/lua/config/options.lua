37fd7d824cd529061e77faa000120d3793e7338c
yaocongchen <yaocongchen@outlook.com>
2025-04-30 23:00:17 +0800

    delete winbar

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,12 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additionlotions here
 local opt = vim.opt
-opt.winbar = "%=%m %f"
+-- opt.winbar = "%=%m %f"
 opt.shiftwidth = 4
 opt.clipboard = "unnamedplus"
 
 local keymap = vim.keymap
-keymap.set('n', '<c-k>', ':wincmd k<CR>')
-keymap.set('n', '<c-j>', ':wincmd j<CR>')
-keymap.set('n', '<c-h>', ':wincmd h<CR>')
-keymap.set('n', '<c-l>', ':wincmd l<CR>')
+keymap.set("n", "<c-k>", ":wincmd k<CR>")
+keymap.set("n", "<c-j>", ":wincmd j<CR>")
+keymap.set("n", "<c-h>", ":wincmd h<CR>")
+keymap.set("n", "<c-l>", ":wincmd l<CR>")
