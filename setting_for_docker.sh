3ee0761b28f9da3d5e0a321b6a1f5cba348c6c17
yaocongchen <yaocongchen@outlook.com>
2025-05-11 14:17:59 +0800

    del export LC_ALL=en_US.UTF-8

--- setting_for_docker.sh
+++ setting_for_docker.sh
@@ -18,7 +18,7 @@ apt-get install -y tmux
 apt update && apt install -y locales
 locale-gen en_US.UTF-8
 export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+# export LC_ALL=en_US.UTF-8
 
 apt-get install -y zip
 
