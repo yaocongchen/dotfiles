1e4088a891a4f7de4d425475d6158abab1252214
yaocongchen <yaocongchen@outlook.com>
2025-08-03 21:07:26 +0800

    新增 ripgrep 和 fd-find 安裝，並調整 lazygit 安裝步驟

--- clearml_init_image.sh
+++ clearml_init_image.sh
@@ -21,7 +21,9 @@ apt-get update && apt-get install -y \
     neovim \
     fontconfig \
     stow \
-    fzf
+    fzf \
+    ripgrep \
+    fd-find
 
 yes '' | add-apt-repository ppa:neovim-ppa/unstable
 apt-get update && apt-get install -y neovim
@@ -74,6 +76,12 @@ mkdir -p ~/.fonts
 cp "$FONT_TMP"/*.ttf ~/.fonts/
 fc-cache -fv
 rm -rf "$FONT_TMP"
+
+# install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+install lazygit -D -t /usr/local/bin/
 
 # install starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y
