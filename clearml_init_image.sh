9de0caadbe8f23d4fac43def90e947a1c5a09459
yaocongchen <yaocongchen@outlook.com>
2025-08-03 18:38:54 +0800

    新增 nvm 安裝步驟，並手動載入 nvm 以確保正確初始化

--- clearml_init_image.sh
+++ clearml_init_image.sh
@@ -32,9 +32,26 @@ huggingface-cli login --token YOUR_HUGGINGFACE_TOKEN
 
 echo 'export HF_HOME=/mnt/shared/cache_storage/huggingface' >> ~/.bashrc
 
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+cat << 'EOF' >> ~/.bashrc
+
+# nvm initialization
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+EOF
+
 # conda
 conda init bash
 source ~/.bashrc
+
+# load nvm manually
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# install node
+nvm install --lts
 
 # time zone
 TZ=Asia/Taipei
