377aeffc8f0a75e08b1b7b9c333209ea9bccc426
yaocongchen <yaocongchen@outlook.com>
2025-08-10 21:30:54 +0800

    清理 lazygit 安裝後的臨時檔案

--- setting.sh
+++ setting.sh
@@ -82,6 +82,7 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
+rm -rf lazygit.tar.gz lazygit
 
 # Install starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y
--- setting_for_docker.sh
+++ setting_for_docker.sh
@@ -92,6 +92,7 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 install lazygit -D -t /usr/local/bin/
+rm -rf lazygit.tar.gz lazygit
 
 # install starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y
